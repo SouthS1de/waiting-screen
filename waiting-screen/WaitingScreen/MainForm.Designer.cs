fd83775b1a5908e571e4973c2999b40f6b7a5d46
Danylo Kushch <dkusch@softserveinc.com>
2020-02-09 16:13:56 +0200

    Initiate project

---
+++ waiting-screen/WaitingScreen/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WaitingScreen
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._screen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this._screen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this._screen.BackColor = System.Drawing.Color.Black;
+            this._screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._screen.Location = new System.Drawing.Point(0, 0);
+            this._screen.Name = "Screen";
+            this._screen.Size = new System.Drawing.Size(800, 450);
+            this._screen.TabIndex = 0;
+            this._screen.TabStop = false;
+            // 
+            // Screen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._screen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this._screen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox _screen;
+    }
+}
+
